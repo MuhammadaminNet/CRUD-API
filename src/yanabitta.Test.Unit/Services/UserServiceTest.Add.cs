82271b40604074e3ff4b5e8ea6eb70b601daee47
Muhammadamin <Muhammadamin@DESKTOP-OA2CG33>
2022-10-13 11:39:31 +0500

    xUnit test for user

---
+++ src/yanabitta.Test.Unit/Services/UserServiceTest.Add.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using src.Data.IRepositories;
+using src.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yanabitta.Test.Unit.Services
+{
+    public partial class UserServiceTest
+    {
+        [Fact]
+        public async ValueTask ShouldCreateUser()
+        {
+            // given
+            var randomUser = CreateRandomUser();
+            var inputUser = randomUser;
+            var expectedUser = inputUser.DeepClone();
+
+            // when
+            user = await userService.CreateAsync(inputUser);
+
+            // then
+            user.Should().NotBeNull();
+
+            user.Name.Should().BeEquivalentTo(expectedUser.Name);
+        }
+    }
+}
--- src/yanabitta.Test.Unit/Services/UserServiceTest.cs
+++ src/yanabitta.Test.Unit/Services/UserServiceTest.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Services.Mappers;
+using src.Data.DbContexts;
+using src.Data.IRepositories;
+using src.Data.Repositories;
+using src.Domain.Entities;
+using src.Services.DTOs;
+using src.Services.IServices;
+using src.Services.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +16,49 @@ using System.Threading.Tasks;
 
 namespace yanabitta.Test.Unit.Services
 {
-    public class UserServiceTest
+    public partial class UserServiceTest
     {
+        private readonly IUnitOfWork unitOfWorkMock;
+        private readonly IUserService userService;
+        private readonly IMapper mapper;
+        private readonly IAttachmentService attachmentService;
+        private readonly UserDbContext dbContext;
+        private User user;
 
+
+        public UserServiceTest()
+        {
+            var options = new DbContextOptionsBuilder<UserDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            this.mapper = new MapperConfiguration(m =>
+            {
+                m.AddProfile<MappingProfile>();
+            }).CreateMapper();
+
+            this.dbContext = new UserDbContext(options);
+            this.unitOfWorkMock = new UnitOfWork(dbContext);
+            this.attachmentService = new AttachmentService(unitOfWorkMock);
+            this.userService = new UserService(unitOfWorkMock,attachmentService,mapper);
+            this.user = new User();
+        }
+
+        private UserForCreation CreateRandomUser()
+            => new UserForCreation()
+            {
+                Name = Faker.Name.First(),
+                Age = Faker.RandomNumber.Next(10, 30),
+                Login = Faker.Name.Last(),
+                Password = Faker.Name.Last(),
+                Role = src.Domain.Enums.UserRole.User
+            };
+
+
+        private Attachment CreateRandomAttachment()
+            => new Attachment()
+            {
+                Name = Faker.Name.First(),
+                Path = Faker.Name.Prefix()
+            };
     }
 }
